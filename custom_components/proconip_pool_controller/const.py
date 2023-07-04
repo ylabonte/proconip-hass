ba9b8b8fa242259775de02937a4783e61a10cb80
Yannic Labonte <yannic.labonte@gmail.com>
2023-07-04 03:46:27 +0200

    Update library package (v1.2.6 --> v.1.2.7) to fix value calculation formula

--- custom_components/proconip_pool_controller/const.py
+++ custom_components/proconip_pool_controller/const.py
@@ -5,7 +5,7 @@ LOGGER: Logger = getLogger(__package__)
 
 NAME = "ProCon.IP Pool Controller"
 DOMAIN = "proconip_pool_controller"
-VERSION = "1.0.1-beta.4"
+VERSION = "1.0.1-beta.5"
 ATTRIBUTION = (
     "Data provided by your Pool Digital ProCon.IP (https://www.pooldigital.de)"
 )
